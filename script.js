35c1818c80355688ba4c12163a707f024c32e7f1
Ismaël Benjara <ib@cezelot.me>
2024-10-15 18:58:37 +0300

    Delete `joke` command
    Delete the `joke` command and load scripts from the site only, rather
    than from a CDN, as sourcehut pages can't execute scripts from third
    party domains.

--- script.js
+++ script.js
@@ -99,8 +99,6 @@ function print_dirs() {
      }).join('\n'));
 }
 
-const url = 'https://v2.jokeapi.dev/joke/Programming';
-
 const commands = {
     ls(dir = null) {
         if (dir) {
@@ -158,33 +156,6 @@ const commands = {
         if (args.length > 0) {
             term.echo(args.join(' '));
         }
-    },
-    async joke() {
-        const res = await fetch(url);
-        const data = await res.json();
-        (async () => {
-            if (data.type == 'twopart') {
-                // set clear the prompt to don't have any
-                // flashing between animations
-                const prompt = this.get_prompt();
-                this.set_prompt('');
-                await this.echo(`Q: ${data.setup}`, {
-                    delay: 50,
-                    typing: true
-                });
-                await this.echo(`A: ${data.delivery}`, {
-                    delay: 50,
-                    typing: true
-                });
-                // restore the prompt
-                this.set_prompt(prompt);
-            } else if (data.type === 'single') {
-                await this.echo(data.joke, {
-                    delay: 50,
-                    typing: true
-                });
-            }
-        })();
     }
 };
 
